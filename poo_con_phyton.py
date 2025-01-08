4da80a4a961d2c3ccbc3e8d4ad30b181f125eb56
Pedrodiaxx <joel.diaz.lopez7@gmail.com>
2025-01-08 11:29:49 -0600

    Creando atributos de la clase

---
+++ poo_con_phyton.py
@@ -0,0 +1,21 @@
+class personaje:
+    nombre = 'Default'
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida= 0
+    #indicar que no se haga nada en este momento
+    pass
+#variable de constructor pasivo de la clase
+mi_personaje = personaje()
+mi_personaje.nombre = "chemafighther"
+mi_personaje. fuerza = 30
+mi_personaje.inteligencia = 12
+mi_personaje.defensa = 28
+mi_personaje.vida = 3
+
+print("el nombre del personaje es", mi_personaje. nombre)
+print("La fuerza de personaje  es", mi_personaje. fuerza)
+print("el inteligencia del personaje es", mi_personaje. inteligencia)
+print("el defensa del personaje es", mi_personaje. defensa)
+print("el vida del personaje es", mi_personaje. vida)
