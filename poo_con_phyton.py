5e1c6c5da09642586f9e9cf3cb8314407b5fcc2a
Pedrodiaxx <joel.diaz.lopez7@gmail.com>
2025-01-10 11:28:43 -0600

    creando el metodo atacar

--- poo_con_phyton.py
+++ poo_con_phyton.py
@@ -1,21 +1,63 @@
 class personaje:
-    nombre = 'Default'
-    fuerza = 0
-    inteligencia = 0
-    defensa = 0
-    vida= 0
-    #indicar que no se haga nada en este momento
-    pass
-#variable de constructor pasivo de la clase
-mi_personaje = personaje()
-mi_personaje.nombre = "chemafighther"
-mi_personaje. fuerza = 30
-mi_personaje.inteligencia = 12
-mi_personaje.defensa = 28
-mi_personaje.vida = 3
+    # nombre = 'Default'
+    # fuerza = 0
+    # inteligencia = 0
+    # defensa = 0
+    # vida= 0
+    #constructor de la clase
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza= fuerza
+        self.inteligencia= inteligencia
+        self.defensa = defensa
+        self.vida = vida
 
-print("el nombre del personaje es", mi_personaje. nombre)
-print("La fuerza de personaje  es", mi_personaje. fuerza)
-print("el inteligencia del personaje es", mi_personaje. inteligencia)
-print("el defensa del personaje es", mi_personaje. defensa)
-print("el vida del personaje es", mi_personaje. vida)
+    def imprimir_atributos(self):
+        print(self.nombre)
+        print("-fuerza:", self. fuerza)
+        print("-inteligencia", self. inteligencia)
+        print("-defensa:", self. defensa)
+        print("-vida:", self. vida)
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+    def esta_vivo(self):
+        return self.vida > 0
+    def morir(self):
+         self.vida < 0
+    def dañar(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    def atacar(self, enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("vida de", enemigo.nombre, "es", enemigo.vida)
+    
+
+            
+#variable de constructor pasivo de la clase
+mi_personaje = personaje("Dante", 100, 3,70, 100)
+mi_personaje.imprimir_atributos
+mi_enemigo = personaje("Vergil", 70, 30,70,100)
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.imprimir_atributos
+
+
+#print(mi_personaje.dañar(mi_enemigo))
+#print(mi_personaje.esta_vivo())
+# mi_personaje.subir_nivel(10,1,5)
+# print("--------------")
+# mi_personaje.imprimir_atributos()
+# mi_personaje.nombre = "chemafighther"
+# mi_personaje. fuerza = 30
+# mi_personaje.inteligencia = 12
+# mi_personaje.defensa = 28
+# mi_personaje.vida = 3
+
+# print("el nombre del personaje es", mi_personaje. nombre)
+# print("La fuerza de personaje  es", mi_personaje. fuerza)
+# print("el inteligencia del personaje es", mi_personaje. inteligencia)
+# print("el defensa del personaje es", mi_personaje. defensa)
+# print("el vida del personaje es", mi_personaje. vida)
